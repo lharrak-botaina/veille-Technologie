61456b938257cba77e27abb4c4f0aabfaab98fc7
lharrak.botaina <lharrak.botaina@gmail.com>
2022-02-24 00:40:33 +0100

    add go

---
+++ GO/HELLOWORLD.go
@@ -0,0 +1,16 @@
+// name of the package every project at least if
+//  you want to be able to execute it needs to have a package called main
+
+package main
+// imports this is where you can import different packages
+import (
+	"fmt"
+)
+// fmt has a function related to input and output
+
+func main() {
+
+	// func main() {} is a function. Any code inside its curly brackets {} will be executed.
+	
+  fmt.Println("Hello World!")
+}
